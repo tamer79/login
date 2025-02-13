746e71f419332c093f2519671a2cb2e3ea7a6021
tamer79 <tamer79@gmail.com>
2025-02-13 20:51:30 +0800

    Correção na conexão com PostgreSQL

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from passlib.context import CryptContext
 
 # URL de conexão com o banco de dados
-DATABASE_URL = "postgresql://postgres:JIIFhrzWyZdvrzOYGdpjdhVQUbALscIf@junction.proxy.rlwy.net:47032/login"
+DATABASE_URL = os.getenv("postgresql://postgres:ZkwJXjxSeeRbgewdfgilpMmxXKUDpBDD@postgres.railway.internal:5432/lili")
 
 # Criando a conexão com o banco de dados
 engine = create_engine(DATABASE_URL)
